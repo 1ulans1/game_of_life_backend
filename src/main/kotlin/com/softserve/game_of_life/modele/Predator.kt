aea74c4060080522592735021ac1b2c4db6aaae5
Anton Vysochin <toxavysochin.03@gmail.com>
2022-12-23 00:13:27 +0200

    add Predator

--- src/main/kotlin/com/softserve/game_of_life/modele/Predator.kt
+++ src/main/kotlin/com/softserve/game_of_life/modele/Predator.kt
@@ -3,93 +3,53 @@ package com.softserve.game_of_life.modele
 import com.softserve.game_of_life.utils.Constants
 
 class Predator(
-    var timeToReproduce: Int = Constants.defaultTimeToReproduce,
-    var timeToFeed: Int = Constants.defaultTimeToFeed,
-    var alive: Boolean = true,
-    val ocean: Ocean, var location: Location
-) : Cell {
-
-    private val random = Random.random
-
-    private val help = listOf(
-        Location(-1, 0),
-        Location(0, -1),
-        Location(0, 1),
-        Location(0, 0),
-        Location(1, 0),
-    )
+    private var timeToReproduce: Int = Constants.randomTimeToReproduce(),
+    private var timeToFeed: Int = Constants.randomDefaultTimeToFeed(),
+    private var alive: Boolean = true,
+    ocean: Ocean,
+    location: Location
+) : Cell(ocean, location) {
 
     override fun getDefaultImage() = "\uD83D\uDFE5"
+
     override fun process() {
         iteration()
-        reproduction()
-        if (alive) {
-            var eatPrey = false
-            help.forEach {
-                val newLocation = normalizeLocation(Location(location.row + it.row, location.colum + it.colum))
 
-                if (ocean.ocean[newLocation.row][newLocation.colum] is Prey) {
-                    changeLocation(newLocation)
-                    eatPrey = true
-                }
-            }
-
-            if (!eatPrey) {
-                val newLocation = normalizeLocation(
-                    Location(
-                        random.nextInt(2) - 1 + location.row,
-                        random.nextInt(2) - 1 + location.colum
-                    )
-                )
-                changeLocation(newLocation)
-            }
-        } else {
-            dead();
+        if (!alive) {
+            dead()
+            return
         }
-    }
 
-    fun reproduction() {
+        if (!eatPrey()) {
+            moveRandom()
+        }
+
         if (timeToReproduce <= 0) {
-            help.shuffled().forEach {
-                val currentLocation = normalizeLocation(Location(it.row + location.row, it.colum + location.colum))
-                if (ocean.ocean[currentLocation.row][currentLocation.colum] is Empty) {
-
-                    ocean.ocean[currentLocation.row][currentLocation.colum] =
-                        Predator(ocean = ocean, location = currentLocation)
-                }
-            }
-            timeToReproduce = Constants.defaultTimeToReproduce
+            reproduce()
+            timeToReproduce = Constants.randomTimeToReproduce()
         }
+
     }
 
-    fun dead() {
-        ocean.ocean[location.row][location.colum] = Empty.instance
+    private fun eatPrey(): Boolean {
+        return neighbors.shuffled().map { normalizeLocation(it + location) }
+            .firstOrNull { ocean.ocean!![it.row][it.colum] is Prey }
+            ?.let {
+                changeLocation(it)
+                timeToFeed = Constants.randomDefaultTimeToFeed()
+                true
+            } ?: false
     }
 
-    private fun normalizeLocation(location: Location): Location {
-        return Location(
-            if (location.row < 0) {
-                ocean.ocean.size - 1
-            } else {
-                location.row % ocean.ocean.size
-            }, if (location.colum < 0) {
-                ocean.ocean[0].size - 1
-            } else {
-                location.colum % ocean.ocean[0].size
-            }
-        )
-    }
-
-    private fun changeLocation(location: Location) {
-        ocean.ocean[this.location.row][this.location.colum] = Empty()
-        ocean.ocean[location.row][location.colum] = this
-        this.location = Location(location.row, location.colum)
-    }
+    override fun createNewCell(location: Location) = Predator(
+        ocean = ocean,
+        location = location
+    )
 
     private fun iteration() {
-
         timeToFeed--
         timeToReproduce--
+
         if (timeToFeed <= 0) {
             alive = false
         }
