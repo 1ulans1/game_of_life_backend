bba18a932221ae5d38af999117e76469c0b75843
maksimblankov <maxblankov436@gmail.com>
2022-12-23 01:10:21 +0200

    refactor Constants

--- src/main/kotlin/com/softserve/game_of_life/utils/Constants.kt
+++ src/main/kotlin/com/softserve/game_of_life/utils/Constants.kt
@@ -1,8 +1,8 @@
 package com.softserve.game_of_life.utils
 
 object Constants {
-    val defaultTimeToReproduce = 7..10
-    val defaultTimeToFeed = 7..10
+    private val defaultTimeToReproduce = 7..10
+    private val defaultTimeToFeed = 7..10
     fun randomTimeToReproduce() = defaultTimeToReproduce.random()
     fun randomDefaultTimeToFeed() = defaultTimeToFeed.random()
 }
