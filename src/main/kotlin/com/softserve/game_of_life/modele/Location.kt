337d8487632c2a2358d97abbf684e4cdd4eb1a4f
Anton Vysochin <toxavysochin.03@gmail.com>
2022-12-23 00:11:39 +0200

    add Location

--- src/main/kotlin/com/softserve/game_of_life/modele/Location.kt
+++ src/main/kotlin/com/softserve/game_of_life/modele/Location.kt
@@ -3,4 +3,6 @@ package com.softserve.game_of_life.modele
 data class Location(
     val row: Int,
     val colum: Int
-)
+) {
+    operator fun plus(other: Location) = Location(row + other.row, colum + other.colum)
+}
