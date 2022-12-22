2e64ca447c170796ee4e0a38a6b92903d630f8b0
Anton Vysochin <toxavysochin.03@gmail.com>
2022-12-23 00:12:12 +0200

    add Ocean

--- src/main/kotlin/com/softserve/game_of_life/modele/Ocean.kt
+++ src/main/kotlin/com/softserve/game_of_life/modele/Ocean.kt
@@ -1,16 +1,16 @@
 package com.softserve.game_of_life.modele
 
-data class Ocean(val ocean: Array<Array<Cell>>) {
+data class Ocean(var ocean: Array<Array<Cell>>? = null) {
 
     fun toOceanDTO(): OceanDTO {
         var predator = 0
         var prey = 0
-        val oceanForDto = ocean
+        val oceanForDto = ocean!!
             .map {
                 it.map { cell ->
                     when (cell) {
                         is Predator -> {
-                            predator++;
+                            predator++
                             1
                         }
 
@@ -34,5 +34,23 @@ data class Ocean(val ocean: Array<Array<Cell>>) {
             prey,
             oceanForDto
         )
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Ocean
+
+        if (ocean != null) {
+            if (other.ocean == null) return false
+            if (!ocean.contentDeepEquals(other.ocean)) return false
+        } else if (other.ocean != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return ocean?.contentDeepHashCode() ?: 0
     }
 }
