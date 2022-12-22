108b2ee071aced920b9ccaa5a2c118981a26f6ea
Anton Vysochin <toxavysochin.03@gmail.com>
2022-12-23 00:14:23 +0200

    add OceanController

--- src/main/kotlin/com/softserve/game_of_life/controller/OceanController.kt
+++ src/main/kotlin/com/softserve/game_of_life/controller/OceanController.kt
@@ -1,6 +1,5 @@
 package com.softserve.game_of_life.controller
 
-import com.softserve.game_of_life.modele.Ocean
 import com.softserve.game_of_life.modele.OceanDTO
 import com.softserve.game_of_life.service.OceanService
 import org.springframework.context.annotation.Bean
@@ -13,33 +12,24 @@ import org.springframework.web.bind.annotation.RestController
 class OceanController(private val service: OceanService) {
 
     @GetMapping
-    fun getOcean() {
-        return service.initOcean()
+    fun getOcean(): OceanDTO {
+        return service.oceanDto()
     }
 
     @GetMapping("/iterate")
     fun iterate(): OceanDTO {
-        service.iteration(0)
-        return service.oceanDto(0)
+        service.iteration()
+        return service.oceanDto()
     }
 
     @GetMapping("/reset")
     fun reset(): OceanDTO {
-        service.reset(0)
-        return service.oceanDto(0)
+        service.reset()
+        return service.oceanDto()
     }
 
     @Bean
     fun output() {
         return service.initOcean()
     }
-//        service.output(0)
-//        service.iteration(0)
-//        service.output(0)
-//        service.iteration(0)
-//        service.output(0)
-//        service.iteration(0)
-//        service.output(0)
-//        service.iteration(0)
-//    }
 }
